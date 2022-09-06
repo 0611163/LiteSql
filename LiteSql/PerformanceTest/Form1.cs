7936905f6c9e83db6ff2210d460e75ba2e4fb787
suxiang <651029594@qq.com>
2022-09-06 16:32:11 +0800

    去掉using

--- LiteSql/PerformanceTest/Form1.cs
+++ LiteSql/PerformanceTest/Form1.cs
@@ -94,10 +94,8 @@ namespace PerformanceTest
             RunTask(() =>
             {
                 Log("删除 开始");
-                using (var session = LiteSqlFactory.GetSession())
-                {
-                    session.DeleteByCondition<SysUser>(string.Format("id>=12"));
-                }
+                var session = LiteSqlFactory.GetSession();
+                session.DeleteByCondition<SysUser>(string.Format("id>=12"));
                 Log("删除 完成");
             });
         }
@@ -113,33 +111,32 @@ namespace PerformanceTest
                 Log("批量修改 开始 count=" + userList.Count);
                 DateTime dt = DateTime.Now;
 
-                using (var session = LiteSqlFactory.GetSession())
+                var session = LiteSqlFactory.GetSession();
+
+                try
                 {
-                    try
+                    session.OnExecuting = (sql, param) =>
                     {
-                        session.OnExecuting = (sql, param) =>
-                        {
-                            Console.WriteLine(sql); //打印SQL
-                        };
+                        Console.WriteLine(sql); //打印SQL
+                    };
 
-                        session.AttachOld(userList);
-                        foreach (SysUser user in userList)
-                        {
-                            user.Remark = "测试修改用户" + _rnd.Next(1, 10000);
-                            user.UpdateUserid = "1";
-                            user.UpdateTime = DateTime.Now;
-                        }
-                        userList.ForEach(item => item.UpdateTime = DateTime.Now);
-
-                        session.BeginTransaction();
-                        session.Update(userList);
-                        session.CommitTransaction();
-                    }
-                    catch (Exception ex)
+                    session.AttachOld(userList);
+                    foreach (SysUser user in userList)
                     {
-                        session.RollbackTransaction();
-                        throw;
+                        user.Remark = "测试修改用户" + _rnd.Next(1, 10000);
+                        user.UpdateUserid = "1";
+                        user.UpdateTime = DateTime.Now;
                     }
+                    userList.ForEach(item => item.UpdateTime = DateTime.Now);
+
+                    session.BeginTransaction();
+                    session.Update(userList);
+                    session.CommitTransaction();
+                }
+                catch (Exception ex)
+                {
+                    session.RollbackTransaction();
+                    throw;
                 }
 
                 string time = DateTime.Now.Subtract(dt).TotalSeconds.ToString("0.000");
@@ -168,24 +165,23 @@ namespace PerformanceTest
                 Log("批量添加 开始 count=" + userList.Count);
                 DateTime dt = DateTime.Now;
 
-                using (var session = LiteSqlFactory.GetSession())
-                {
-                    try
-                    {
-                        session.OnExecuting = (sql, param) =>
-                        {
-                            Console.WriteLine(sql); //打印SQL
-                        };
+                var session = LiteSqlFactory.GetSession();
 
-                        session.BeginTransaction();
-                        session.Insert(userList);
-                        session.CommitTransaction();
-                    }
-                    catch
+                try
+                {
+                    session.OnExecuting = (sql, param) =>
                     {
-                        session.RollbackTransaction();
-                        throw;
-                    }
+                        Console.WriteLine(sql); //打印SQL
+                    };
+
+                    session.BeginTransaction();
+                    session.Insert(userList);
+                    session.CommitTransaction();
+                }
+                catch
+                {
+                    session.RollbackTransaction();
+                    throw;
                 }
 
                 string time = DateTime.Now.Subtract(dt).TotalSeconds.ToString("0.000");
@@ -204,36 +200,36 @@ namespace PerformanceTest
                 Log("循环修改 开始 count=" + userList.Count);
                 DateTime dt = DateTime.Now;
 
-                using (var session = LiteSqlFactory.GetSession())
+                var session = LiteSqlFactory.GetSession();
+
+                try
                 {
-                    try
+                    session.OnExecuting = (sql, param) =>
                     {
-                        session.OnExecuting = (sql, param) =>
-                        {
-                            Console.WriteLine(sql); //打印SQL
-                        };
+                        Console.WriteLine(sql); //打印SQL
+                    };
 
-                        session.AttachOld(userList);
-                        foreach (SysUser user in userList)
-                        {
-                            user.Remark = "测试修改用户" + _rnd.Next(1, 10000);
-                            user.UpdateUserid = "1";
-                            user.UpdateTime = DateTime.Now;
-                        }
-
-                        session.BeginTransaction();
-                        foreach (SysUser user in userList)
-                        {
-                            session.Update(user);
-                        }
-                        session.CommitTransaction();
-                    }
-                    catch
+                    session.AttachOld(userList);
+                    foreach (SysUser user in userList)
                     {
-                        session.RollbackTransaction();
-                        throw;
+                        user.Remark = "测试修改用户" + _rnd.Next(1, 10000);
+                        user.UpdateUserid = "1";
+                        user.UpdateTime = DateTime.Now;
                     }
+
+                    session.BeginTransaction();
+                    foreach (SysUser user in userList)
+                    {
+                        session.Update(user);
+                    }
+                    session.CommitTransaction();
                 }
+                catch
+                {
+                    session.RollbackTransaction();
+                    throw;
+                }
+
 
                 string time = DateTime.Now.Subtract(dt).TotalSeconds.ToString("0.000");
                 Log("循环修改 完成，耗时：" + time + "秒");
@@ -260,27 +256,26 @@ namespace PerformanceTest
                 Log("循环添加 开始 count=" + userList.Count);
                 DateTime dt = DateTime.Now;
 
-                using (var session = LiteSqlFactory.GetSession())
-                {
-                    try
-                    {
-                        session.OnExecuting = (sql, param) =>
-                        {
-                            Console.WriteLine(sql); //打印SQL
-                        };
+                var session = LiteSqlFactory.GetSession();
 
-                        session.BeginTransaction();
-                        foreach (SysUser user in userList)
-                        {
-                            session.Insert(user);
-                        }
-                        session.CommitTransaction();
-                    }
-                    catch
+                try
+                {
+                    session.OnExecuting = (sql, param) =>
                     {
-                        session.RollbackTransaction();
-                        throw;
+                        Console.WriteLine(sql); //打印SQL
+                    };
+
+                    session.BeginTransaction();
+                    foreach (SysUser user in userList)
+                    {
+                        session.Insert(user);
                     }
+                    session.CommitTransaction();
+                }
+                catch
+                {
+                    session.RollbackTransaction();
+                    throw;
                 }
 
                 string time = DateTime.Now.Subtract(dt).TotalSeconds.ToString("0.000");
@@ -299,19 +294,18 @@ namespace PerformanceTest
 
                 for (int i = 0; i < 10; i++)
                 {
-                    using (var session = LiteSqlFactory.GetSession())
-                    {
-                        ISqlString sql = session.CreateSql(@"
-                            select t.* 
-                            from sys_user t 
-                            where t.id > @id 
-                            and t.real_name like @remark", 20, "%测试%");
+                    var session = LiteSqlFactory.GetSession();
 
-                        sql.Append(" order by t.create_time desc, t.id asc");
+                    ISqlString sql = session.CreateSql(@"
+                        select t.* 
+                        from sys_user t 
+                        where t.id > @id 
+                        and t.real_name like @remark", 20, "%测试%");
 
-                        List<SysUser> userList = sql.QueryList<SysUser>();
-                        Log("查询结果 count=" + userList.Count.ToString());
-                    }
+                    sql.Append(" order by t.create_time desc, t.id asc");
+
+                    List<SysUser> userList = sql.QueryList<SysUser>();
+                    Log("查询结果 count=" + userList.Count.ToString());
                 }
 
                 string time = DateTime.Now.Subtract(dt).TotalSeconds.ToString("0.000");
@@ -333,24 +327,23 @@ namespace PerformanceTest
                     int total = m_SysUserDal.GetTotalCount();
                     int pageSize = 100;
                     int pageCount = (total - 1) / pageSize + 1;
-                    using (var session = LiteSqlFactory.GetSession())
+                    var session = LiteSqlFactory.GetSession();
+
+                    List<SysUser> userList = new List<SysUser>();
+                    for (int page = 1; page <= pageCount; page++)
                     {
-                        List<SysUser> userList = new List<SysUser>();
-                        for (int page = 1; page <= pageCount; page++)
-                        {
-                            ISqlString sql = session.CreateSql(@"
-                                select t.* 
-                                from sys_user t 
-                                where 1=1 
-                                and t.id > @id 
-                                and t.real_name like @remark", 20, "%测试%");
+                        ISqlString sql = session.CreateSql(@"
+                            select t.* 
+                            from sys_user t 
+                            where 1=1 
+                            and t.id > @id 
+                            and t.real_name like @remark", 20, "%测试%");
 
-                            string orderby = " order by t.create_time desc, t.id asc";
+                        string orderby = " order by t.create_time desc, t.id asc";
 
-                            userList.AddRange(sql.QueryPage<SysUser>(orderby, pageSize, page));
-                        }
-                        Log("分页查询结果 count=" + userList.Count.ToString());
+                        userList.AddRange(sql.QueryPage<SysUser>(orderby, pageSize, page));
                     }
+                    Log("分页查询结果 count=" + userList.Count.ToString());
                 }
 
                 string time = DateTime.Now.Subtract(dt).TotalSeconds.ToString("0.000");
@@ -372,19 +365,18 @@ namespace PerformanceTest
                 {
                     Task task = RunTask(() =>
                     {
-                        using (var session = LiteSqlFactory.GetSession())
-                        {
-                            ISqlString sql = session.CreateSql(@"
+                        var session = LiteSqlFactory.GetSession();
+
+                        ISqlString sql = session.CreateSql(@"
                             select t.* 
                             from sys_user t 
                             where t.id > @id 
                             and t.real_name like @remark", 20, "%测试%");
 
-                            sql.Append(" order by t.create_time desc, t.id asc");
+                        sql.Append(" order by t.create_time desc, t.id asc");
 
-                            List<SysUser> userList = sql.QueryList<SysUser>();
-                            Log("查询结果 count=" + userList.Count.ToString());
-                        }
+                        List<SysUser> userList = sql.QueryList<SysUser>();
+                        Log("查询结果 count=" + userList.Count.ToString());
                     });
                     tasks.Add(task);
                 }
